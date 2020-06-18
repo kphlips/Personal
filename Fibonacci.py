7b0fcbb9282615643dc7934522fdb4dc9eb37dda
kphlips <66272415+kphlips@users.noreply.github.com>
2020-06-18 13:31:08 -0400

    Add Fibonacci

---
+++ Fibonacci.py
@@ -0,0 +1,9 @@
+a = 0 #The first of a pair of Fibonacci numbers
+b = 1 # The second
+temp = 0 # Temporary variable for storing the next number in the sequence
+limit = int(input("Enter the maximum Fibonacci number: ")) # Record user input
+while a <= limit: #Ensures that the number printed will not exceed the limit
+        print(a)
+        temp = a + b # Creating the next number in the sequence
+        a = b #The new first numnber becomes the old second number
+        b = temp #The new second number is the sum of the old two numbers
