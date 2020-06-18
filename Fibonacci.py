dc80384d73f9e376dc84a95947c48828e3b5cbb7
Kevin Phlips <66272415+kphlips@users.noreply.github.com>
2020-06-18 14:55:37 -0400

    Add negative number loop to Fibonacci.py

--- Fibonacci.py
+++ Fibonacci.py
@@ -2,6 +2,8 @@ a = 0 #The first of a pair of Fibonacci numbers
 b = 1 # The second
 temp = 0 # Temporary variable for storing the next number in the sequence
 limit = int(input("Enter the maximum Fibonacci number: ")) # Record user input
+while limit <0:
+	limit = int(input("Must be a positive number. Enter the maximum Fibonacci number: "))
 while a <= limit: #Ensures that the number printed will not exceed the limit
         print(a)
         temp = a + b # Creating the next number in the sequence
